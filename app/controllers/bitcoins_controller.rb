d295e2432a6dffd30f84f1a10b711d51022daabf
Max Lehman <max.lehman@kellogg.northwestern.edu>
2025-05-13 15:53:35 +0000

    class

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -1,6 +1,16 @@
 class BitcoinsController < ApplicationController
 
   def index
+    @bitcoin = params["amount"].to_f
+    require "net/http"
+    require "json"
+    url = "https://blockchain.info/ticker"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    bitcoin_data = JSON.parse(response)
+
+    @usd_rate = bitcoin_data["USD"]["last"]
+    @usd_value = @usd_rate * @bitcoin
     # add backend code
     # render bitcoins/index view
   end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,12 +1,15 @@
 class CompaniesController < ApplicationController
 
   def index
+    @companies = Company.all
     # find all Company rows
     # render companies/index view
   end
 
   def show
     # find a Company
+    @company = Company.find_by({"id" => params["id"]})
+    p @company 
     # render companies/show view with details about Company
   end
 
@@ -14,12 +17,15 @@ class CompaniesController < ApplicationController
     # render view with new Company form
   end
 
-  # def create
-  #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+  def create
+    @company = Company.new 
+    @company["name"] = params["name"]
+    @company["city"] = params["city"]
+    @company["state"] = params["state"]
+    @company.save
+
+    redirect_to "/companies"
+  end
 
   # def edit
   #   # find a Company
